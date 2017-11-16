1b8b1f1e350e245ff89a0e99979cab4c00649aaa
Cian Gannon <lego@live.ie>
2017-11-16 22:28:56 +0000

    Gerry Adams Added

--- eliza_helpers.go
+++ eliza_helpers.go
@@ -1,11 +1,50 @@
 package eliza
 
 import (
+    "fmt"
     "math/rand"
+    "regexp"
+    "strings"
 )
 
-func ElizaIntro() string {
-    return randChoice(Introductions)
+func ReplyTo(statement string) string {
+    statement = preprocess(statement)
+
+    for pattern, responses := range Psychobabble {
+        re := regexp.MustCompile(pattern)
+        matches := re.FindStringSubmatch(statement)
+
+        if len(matches) > 0 {
+            var fragment string
+            if len(matches) > 1 {
+                fragment = reflect(matches[1])
+            }
+
+            response := randChoice(responses)
+            if strings.Contains(response, "%s") {
+                response = fmt.Sprintf(response, fragment)
+            }
+            return response
+        }
+    }
+
+    return randChoice(DefaultResponses)
+}
+
+func preprocess(statement string) string {
+    statement = strings.TrimRight(statement, "\n.!")
+    statement = strings.ToLower(statement)
+    return statement
+}
+
+func reflect(fragment string) string {
+    words := strings.Split(fragment, " ")
+    for i, word := range words {
+        if Reflected, ok := Reflected[word]; ok {
+            words[i] = Reflected
+        }
+    }
+    return strings.Join(words, " ")
 }
 
 func randChoice(list []string) string {
--- eliza_script.go
+++ eliza_script.go
@@ -1,9 +1,35 @@
 package eliza
 
-// A list of introduction sentences for ELIZA.
-var Introductions = []string{
-    "Hello. How are you feeling today?",
-    "How do you do. Please tell me your problem.",
-    "Please tell me what's been bothering you.",
-    "Is something troubling you?",
+var Psychobabble = map[string][]string{
+    `i need (.*)`: {
+        "Do you really %s? there is occupied counties to liberate!",
+        "Why do you need %s when you could vote Sinn Fein?",
+    },
+}
+
+var DefaultResponses = []string{
+    "Can we talk about the 32 county republic",
+    "Let's focus on whats important.... British occupaption forces in Northern Ireland",
+    "Did you know 26 + 6 = 1, it's true!",
+    "Very interesting.",
+    "Do you have time to talk about Wolfe Tone?",
+    "Follow me on twitter @GerryAdamsSF",
+    "Have you voted Sinn Fein?",
+}
+
+var Reflected = map[string]string{
+    "am": "are",
+    "was": "were",
+    "i": "you",
+    "i'd": "you would",
+    "i've": "you have",
+    "i'll": "you will",
+    "my": "your",
+    "are": "am",
+    "you've": "I have",
+    "you'll": "I will",
+    "your": "my",
+    "yours": "mine",
+    "you": "me",
+    "me": "you",
 }
