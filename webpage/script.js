e21663cc18b32dc05d61da41516aae432c37e1b4
Cian Gannon <lego@live.ie>
2017-11-17 18:27:02 +0000

    Commented and formatted

--- webpage/script.js
+++ webpage/script.js
@@ -14,22 +14,26 @@ inputText.keypress(function(e){
     // Prevents page refresh
     e.preventDefault();
 
+    // Variable Setup
     const userInput = inputText.val();
     inputText.val("");
-    
+    const userPrompt = {"input" : userInput }
     ouputText.append("<p>" + "<b>User: </b>" + userInput + "</p>");
     
-    const queryParams = {"input" : userInput }
-    
     // When go can comunicate with webpage '.done' will run otherwise '.fail' will
-    $.get("/elizaPrompt", queryParams)
-        .done(resp => {
-            
-            const output = "<p><b>Bot: </b>" + resp + "</p>";
+    $.get("/elizaPrompt", userPrompt)
+        .done(response => {
+            // if the webpage can connect to the GO eliza files then the user will be prompted with a response from eliza
+            const output = "<p><b>Bot: </b>" + response + "</p>";
+
+            // The bots response with HTML elements are output to the webpage with an alert
             setTimeout(function(){ouputText.append(output), new Audio('assets/sound/responseAlert.mp3').play();}, 1000);
             
         }).fail(() => {
-            const output = "<p><b>Bot is offline</b> </p>";
+            // If the webpage cannot retrieve the GO eliza files the user will be informed
+            const output = "<p><b>ALERT: </b>Bot is offline</p>";
+
+            // An alert is ouput with HTML elements informing the user
             setTimeout(function(){ouputText.append(output), new Audio('assets/sound/responseAlert.mp3').play();}, 1000);
         });
 });
